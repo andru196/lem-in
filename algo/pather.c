70577be60684f83389107bd20d0c5974d7f3e196
Spider worker Falia flowers <sfalia-f@mi-a4.21-school.ru>
2019-10-24 22:57:13 +0300

    kek

--- algo/pather.c
+++ algo/pather.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pather.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: andru196 <andru196@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sfalia-f <sfalia-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/19 15:44:27 by sfalia-f          #+#    #+#             */
-/*   Updated: 2019/10/23 22:10:24 by andru196         ###   ########.fr       */
+/*   Updated: 2019/10/24 22:56:55 by sfalia-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,37 @@ void	move_nxt(t_room *frst, t_room *end)
 }
 
 /*
+** уничтожает неугодных
+*/
+
+void	destroy_path(t_room *r)
+{
+	t_lst	*l;
+	int		fl;
+	t_room	*from;
+
+	
+	fl = 1;
+	while (fl)
+	{
+		fl = 0;
+		l = r->tubes;
+		while (l)
+		{
+			if ((from = (other_room(l->cont, r)))->path == r->path)
+			{
+				((t_tube *)l->cont)->status = -1;
+				r->parent = NULL;
+				r->path = -1;
+				fl = 1;
+				r = from;
+			}
+			l = l->nxt;
+		}
+	}
+}
+
+/*
 ** собираем маршруты в массивы
 */
 
@@ -96,7 +127,7 @@ void	make_path(t_cont *c)
 	c->paths = ft_array(1);
 	while (l)
 	{
-		if (((r = other_room(((t_tube *)l->cont), c->end))->parent != start && r->parent) || r == c->end)
+		if (((r = other_room(((t_tube *)l->cont), c->start))->parent != start && r->parent) || r == c->end)
 		{
 			rms = ft_array(10);
 			r = other_room(l->cont, c->end);
@@ -110,6 +141,32 @@ void	make_path(t_cont *c)
 				ft_array_push(rms, c->end);
 			ft_array_push(c->paths, rms);
 		}
+		else
+			destroy_path(other_room(((t_tube *)l->cont), c->start));
+		l = l->nxt;
+	}
+}
+
+void	after_makepath(t_cont *c)
+{
+	int 	i;
+	t_lst	*l;
+	int		f;
+	t_room	*r;
+
+	l = c->end->tubes;
+	while (l)
+	{
+		i = -1;
+		f = 1;
+		r = other_room(l->cont, c->end);
+		while (++i < c->paths->len)
+		{
+			if (c->paths->path[i]->room[c->paths->path[i]->len - 2] == r)
+				f = 0;
+		}
+		if (f)
+			destroy_path(r);
 		l = l->nxt;
 	}
 }
@@ -117,6 +174,8 @@ void	make_path(t_cont *c)
 /*
 ** связываем путь со старта и конца
 ** разворачивая родительскую связь
+** фром - комната с концевого маршрута
+** r - комната с начала
 */
 
 void	set_connection(t_room *r, t_room *from, int path)
@@ -161,8 +220,9 @@ void	del_links_mod(t_tube *frst)
 }
 
 /*
-** строим пути с энжа
+** строим пути с энжa
 */
+
 void	from_bottom(t_room *end, t_room *start)
 {
 	t_lst	*l;
@@ -186,12 +246,86 @@ void	from_bottom(t_room *end, t_room *start)
 	}
 }
 
+void	room_disconnect(t_room *r)
+{
+	t_lst	*l;
+	t_room	*or;
+	t_tube	*t;
+
+	l = r->tubes;
+	while (l)
+	{
+		t = l->cont;
+		if (!t->status)
+		{
+			or = other_room(t, r);
+			if (or->path != r->path)
+				t->status = -1;
+		}
+	}
+}
+
 /*
-** вызовет функцию связывания, если встречена связь старта и конца
+** устанавливает связь если у комнаты со стартового маршрута нет других не деактивированных связей
+** r - комната с начала имеющая связь с комнатой с конца
 */
-int		num_of_links(t_room *r)
+int		make_if_start_one(t_room *r)
+{
+	t_lst	*l;
+	t_tube	*t;
+	t_room	*or;
+
+	l = r->tubes;
+	while (l)
+	{
+		t = l->cont;
+		if (t->status == 0)
+		{
+			or = other_room(t, r);
+			if (or->path > MID)
+			{
+				set_connection(r, or, or->path);
+				room_disconnect(or);
+			}
+		}
+	}
+	return (0);
+}
+
+/*
+** устанавливает связь, если у финишного маршрута нет других связей
+** (как в функции сверху, лень делать универсальную, да и хуй с ним, пир все равно питух)
+*/
+int		make_if_end_one(t_room *r)
+{
+	t_lst	*l;
+	t_tube	*t;
+	t_room	*or;
+
+	l = r->tubes;
+	while (l)
+	{
+		t = l->cont;
+		if (t->status == 0)
+		{
+			or = other_room(t, r);
+			if (or->path < MID && or->path != -1)
+			{
+				set_connection(or, r, or->path);
+				room_disconnect(or);
+			}
+		}
+	}
+	return (0);
+}
+
+/*
+** посчитает, можно ли связывать
+*/
+int		num_of_links_mod(t_room *r)
 {
 	int		rez1;
+	int		rez2;
 	int		rez0;
 	t_lst	*l;
 	t_tube	*t;
@@ -200,22 +334,26 @@ int		num_of_links(t_room *r)
 	l = r->tubes;
 	rez0 = 0;
 	rez1 = 0;
+	rez2 = 0;
 	while (l)
 	{
 		t = (t_tube *)l->cont;
 		or = other_room(t, r);
 		if (r->path > -1 && or->path > -1 && ((r->path > MID && or->path < MID)
 		|| (or->path > MID && r->path < MID)))
-			return (0);//тут связывание и разрыв лишних связей
+				rez2++;
 		if (t->status == 0)
 			rez0++;
 		else if (t->status == 1)
 			rez1++;
 		l = l->nxt;
 	}
-	return (rez0 == 1 && rez1 == 1);
+	if (rez2 == 1 && rez1 == 1 && !rez0 && MID < r->path)
+		return (make_if_end_one(r));
+	if (rez2 == 1 && rez1 == 1 && !rez0 && MID > r->path && r->path > -1)
+		return (make_if_start_one);
+	return (rez0 == 1 && rez1 == 1 && !rez2);
 }
-
 
 void	move_nxt_mod(t_cont *c)
 {
@@ -236,7 +374,6 @@ void	path(t_cont *c)
 	t_lst	*l;
 	t_room	*r;
 	t_tube	*t;
-	//int		f;
 
 	path = 0;
 	l = c->start->tubes;
@@ -256,7 +393,8 @@ void	path(t_cont *c)
 	}
 	del_links(c->tubes);
 	move_nxt(c->rooms, c->end);
-	make_path(c);
 	from_bottom(c->end, c->start);
-	del_links(c->tubes);
+	del_links_mod(c->tubes);
+	make_path(c);
+	after_makepath(c);
 }
